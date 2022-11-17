6d76e1497ba4a555f5c904f209b387a12482d547
João Amaral <joao.amaral.almeida99@gmail.com>
2022-11-16 22:48:50 -0300

    feat: add config theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
       fontFamily: {
         sans: "Roboto, sans-serif",
       },
+
+      colors: {
+        gray: {
+          100: "#29292E",
+          200: "#E1E1E6",
+          500: "#202024",
+          900: "#121214",
+        },
+      },
+      placeholderColor: {
+        primary: "#87868B",
+      },
     },
   },
   plugins: [],
